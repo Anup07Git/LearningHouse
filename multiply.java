f7ce5d911753d10909efbc282c099cd1c8082a12
Anup Singh <recanup07@gmail.com>
2022-03-07 21:17:31 +0530

    add multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+        public static void main(String[] args){
+            int a = 0,b =5;
+            int mul =0;
+            mul = a*b;
+            System.out.println(mul);
+        }
+    }
