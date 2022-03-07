6f9353f92a66304028673e93a46859d31540abcd
Anup Singh <recanup07@gmail.com>
2022-03-07 21:06:31 +0530

    Removed space

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum{
     public static void main(String[] args){
         int a = 0,b =5;
         int sum =0;
-        sum = a +b;
+        sum = a+b;
         System.out.println(sum);
     }
 }
